f60c412eb8a8b63162395c442c3b6e18defc20c4
Henrique Breim <henrique@breim.com.br>
2026-08-28 00:30:45 +0200

    test: block real HTTP requests in specs with WebMock
    Specs exercise the registry client through an in-memory
    FakeRegistryHttp, but nothing prevented a spec from accidentally
    reaching the real registry. WebMock.disable_net_connect! turns that
    into a deterministic failure instead of a network-dependent flake.

--- ruby-skills.gemspec
+++ ruby-skills.gemspec
@@ -47,5 +47,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rubocop", "~> 1.58"
   spec.add_development_dependency "rubocop-performance", "~> 1.20"
   spec.add_development_dependency "rubocop-rspec", "~> 3.0"
+  spec.add_development_dependency "webmock", "~> 3.26"
   spec.add_development_dependency "yard", "~> 0.9"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,16 @@ require "fileutils"
 require "json"
 require "pathname"
 require "tmpdir"
+require "webmock/rspec"
 require "yaml"
 
 Dir[File.expand_path("support/**/*.rb", __dir__)].each do |path|
   require path
 end
+
+# Specs talk to the registry through RubySkillsSpec::FakeRegistryHttp; any real
+# HTTP call is a bug, so fail it deterministically instead of hitting the network.
+WebMock.disable_net_connect!
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = "spec/examples.txt"
